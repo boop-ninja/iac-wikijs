503ff098114ebcbce0179e8b4ad942cc9834cda3
MBRound18 <12646562+mbround18@users.noreply.github.com>
2024-02-21 20:00:43 -0800

    revised deployment

--- versions.tf
+++ versions.tf
@@ -4,5 +4,18 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.20.0"
     }
+
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
   }
+}
+
+variable "cloudflare_api_token" {
+  default = ""
+}
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
