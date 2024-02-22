a4b2b5079c2a0b430733b32949c7ca8ec1be56c4
MBRound18 <12646562+mbround18@users.noreply.github.com>
2024-02-21 22:43:57 -0800

    Moved files apart

--- main.tf
+++ main.tf
@@ -16,27 +16,6 @@ resource "kubernetes_namespace" "i" {
 
 
 
-##################################################################
-# Persisted Volume Claims
-##################################################################
-
-resource "kubernetes_persistent_volume_claim" "i" {
-  metadata {
-    name      = "${var.namespace}-database-pv-claim"
-    namespace = var.namespace
-    labels    = local.common_labels
-  }
-  spec {
-    storage_class_name = "longhorn"
-    access_modes       = ["ReadWriteOnce"]
-    resources {
-      requests = {
-        storage = "20Gi"
-      }
-    }
-  }
-}
-
 
 
 
