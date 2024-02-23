89a7454f4b246b59dfb92f69e7ee2adc408b4be1
MBRound18 <12646562+mbround18@users.noreply.github.com>
2024-02-22 16:50:01 -0800

    inno cluster

--- stateful_set.tf
+++ stateful_set.tf
@@ -1,89 +1,114 @@
-# Database Deployment
-resource "kubernetes_stateful_set" "d" {
-  metadata {
-    name = "${var.namespace}-database"
-    labels = merge(local.common_labels, {
-      app    = "database"
-      engine = "mysql"
-    })
-    namespace = var.namespace
-  }
+## Database Deployment
+#resource "kubernetes_stateful_set" "d" {
+#  metadata {
+#    name = "${var.namespace}-database"
+#    labels = merge(local.common_labels, {
+#      app    = "database"
+#      engine = "mysql"
+#    })
+#    namespace = var.namespace
+#  }
+#
+#  spec {
+#    selector {
+#      match_labels = local.common_labels
+#    }
+#
+#    template {
+#      metadata {
+#        labels = merge(local.common_labels, {
+#          app = "database"
+#        })
+#      }
+#
+#      spec {
+#        container {
+#
+#          name  = "${var.namespace}-database"
+#          image = var.docker_images.database
+#
+#          env {
+#            name  = "MYSQL_ALLOW_EMPTY_PASSWORD"
+#            value = "1"
+#          }
+#
+#          env_from {
+#            config_map_ref {
+#              name = kubernetes_config_map.i_db.metadata.0.name
+#            }
+#          }
+#
+#          env_from {
+#            secret_ref {
+#              name = kubernetes_secret.i_db.metadata.0.name
+#            }
+#          }
+#
+#          env_from {
+#            secret_ref {
+#              name = kubernetes_secret.i_db_root.metadata.0.name
+#            }
+#          }
+#
+#          resources {
+#            limits = {
+#              cpu    = "0.5"
+#              memory = "1Gi"
+#            }
+#          }
+#
+#          port {
+#            name           = "database"
+#            container_port = local.database_port
+#          }
+#
+#          volume_mount {
+#            name       = local.database_volume_name
+#            mount_path = "/var/lib/mysql"
+#            sub_path   = "data"
+#            read_only  = false
+#          }
+#          volume_mount {
+#            name       = local.database_volume_name
+#            mount_path = "/etc/mysql/conf.d"
+#            sub_path   = "conf"
+#          }
+#        }
+#
+#        volume {
+#          name = local.database_volume_name
+#
+#          persistent_volume_claim {
+#            claim_name = local.pvc_name
+#          }
+#        }
+#      }
+#    }
+#    service_name = kubernetes_service.i_database.metadata.0.name
+#  }
+#}
 
-  spec {
-    selector {
-      match_labels = local.common_labels
+
+
+resource "kubernetes_manifest" "wikijs_innodbcluster" {
+  manifest = {
+    apiVersion = "mysql.oracle.com/v2"
+    kind       = "InnoDBCluster"
+    metadata = {
+      name      = "wikijs-innodbcluster"
+      namespace = "wikijs"
     }
-
-    template {
-      metadata {
-        labels = merge(local.common_labels, {
-          app = "database"
-        })
-      }
-
-      spec {
-        container {
-
-          name  = "${var.namespace}-database"
-          image = var.docker_images.database
-
-          env {
-            name  = "MYSQL_ALLOW_EMPTY_PASSWORD"
-            value = "1"
-          }
-
-          env_from {
-            config_map_ref {
-              name = kubernetes_config_map.i_db.metadata.0.name
-            }
-          }
-
-          env_from {
-            secret_ref {
-              name = kubernetes_secret.i_db.metadata.0.name
-            }
-          }
-
-          env_from {
-            secret_ref {
-              name = kubernetes_secret.i_db_root.metadata.0.name
-            }
-          }
-
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "1Gi"
-            }
-          }
-
-          port {
-            name           = "database"
-            container_port = local.database_port
-          }
-
-          volume_mount {
-            name       = local.database_volume_name
-            mount_path = "/var/lib/mysql"
-            sub_path   = "data"
-            read_only  = false
-          }
-          volume_mount {
-            name       = local.database_volume_name
-            mount_path = "/etc/mysql/conf.d"
-            sub_path   = "conf"
-          }
-        }
-
-        volume {
-          name = local.database_volume_name
-
-          persistent_volume_claim {
-            claim_name = local.pvc_name
-          }
-        }
+    spec = {
+      instances =  1
+      secretName = kubernetes_secret.i_db_root.metadata.0.name
+      version = "8.3"
+      mycnf = <<-EOT
+        [mysqld]
+        max_connections =  200
+        EOT
+      service = {
+        type = "ClusterIP"
       }
     }
-    service_name = kubernetes_service.i_database.metadata.0.name
   }
 }
