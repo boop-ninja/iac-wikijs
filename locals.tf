204946010237380f983801d33e5309ab3c83dad2
MBRound18 <12646562+mbround18@users.noreply.github.com>
2024-02-22 17:15:17 -0800

    inno cluster

--- locals.tf
+++ locals.tf
@@ -4,6 +4,6 @@ locals {
   common_labels = {
     target = var.namespace
   }
-  database_host = format("%s.%s.svc.cluster.local", kubernetes_service.i_database.metadata[0].name, var.namespace)
+  database_host = format("%s.%s.svc.cluster.local", "wikijs-innodbcluster", var.namespace)
   database_port = 3306
 }
