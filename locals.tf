e68af54fa5dd7d2f0a87ad4ab5fc7abcc78decaf
MBRound18 <12646562+mbround18@users.noreply.github.com>
2024-02-23 16:42:22 -0800

    bind

--- locals.tf
+++ locals.tf
@@ -1,6 +1,4 @@
 locals {
-  pvc_name             = kubernetes_persistent_volume_claim.i.metadata.0.name
-  database_volume_name = "${var.namespace}-database-persistent-storage"
   common_labels = {
     target = var.namespace
   }
